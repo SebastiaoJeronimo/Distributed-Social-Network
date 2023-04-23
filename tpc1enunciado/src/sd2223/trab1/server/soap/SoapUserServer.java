7078c6dea7d6caedcb6a8c9f5e9af168847893e8
Sebastiao <sc.jeronimo@campus.fct.unl.pt>
2023-04-23 16:43:41 +0100

    added soapUserServer be aware of the serviceDomain may be useful to the constructor

---
+++ tpc1enunciado/src/sd2223/trab1/server/soap/SoapUserServer.java
@@ -0,0 +1,38 @@
+package sd2223.trab1.server.soap;
+
+
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jakarta.xml.ws.Endpoint;
+import sd2223.trab1.utilities.Discovery;
+
+public class SoapUserServer {
+
+	public static final int PORT = 8081;
+	public static final String SERVICE = "users";
+	public static String SERVER_BASE_URI = "http://%s:%s/soap";
+
+	private static final Logger Log = Logger.getLogger(SoapUserServer.class.getName());
+
+	public static void main(String[] args) throws Exception {
+
+		String serviceDomain = args[0];
+//		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+//		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+//		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+//		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+
+		Log.setLevel(Level.INFO);
+
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
+
+		Discovery discovery = Discovery.getInstance();
+		discovery.announce(serviceDomain, SERVICE, serverURI); //checkar o discovery
+		Endpoint.publish(serverURI.replace(ip, "0.0.0.0"), new SoapUsersWebService(serviceDomain)); //checkar construtor
+
+		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE, serverURI));
+	}
+}
